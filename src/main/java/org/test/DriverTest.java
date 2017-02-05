7700bcb605ef8f0235abac9c870f8aa3a4985d9c
freyjaj93 <fjkulsdt@unb.ca>
2017-02-05 12:00:02 -0500

    First Commit

---
+++ src/main/java/org/test/DriverTest.java
@@ -0,0 +1,24 @@
+package main.java.org.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by freyjaj93 on 2017-02-04.
+ */
+class DriverTest {
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void run() {
+
+    }
+
+}
