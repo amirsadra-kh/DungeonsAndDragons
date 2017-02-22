738548801a495105467de84f99c2da528ef3b40a
Amir Sadra Khorramizadeh <amirsadra@me.com>
2017-02-22 02:03:31 -0500

    Modified ItemEnum for complete set of items.

--- src/main/java/org/model/ItemEnum.java
+++ src/main/java/org/model/ItemEnum.java
@@ -1,8 +1,10 @@
 package main.java.org.model;
 
 /**
- * Created by mmokarian on 1/21/17.
+ * Created by Amir Sadra on 2/22/17.
+ * HELMET = 0, ARMOR = 1, SHIELD = 2, RING = 3, BELT = 4, BOOTS = 5, WEAPON = 6
  */
+
 public enum ItemEnum {
-    ARMOR, SHEILD, WEAPON, BOOTS, RING, HELMET
+    HELMET, ARMOMR, SHIELD, RING, BELT, BOOTS, WEAPON
 }
