03e0c417b503d228837cd0e579192f7bc1ff0e54
Amir Sadra Khorramizadeh <amirsadra@me.com>
2017-02-22 02:13:50 -0500

    Completed Item class for creating/editing items.

--- src/main/java/org/model/Item.java
+++ src/main/java/org/model/Item.java
@@ -1,16 +1,16 @@
 package main.java.org.model;
 
-import main.java.org.model.ItemEnum;
-
 import java.awt.*;
 
 public class Item {
 
     ItemEnum item;
+    AbilityEnum ability;
+    int enhance;
     Point coordinate;
 
-    public Item(ItemEnum item, Point coordinate) {
-        this.item = item;
+    public Item(ItemEnum item,AbilityEnum ability ,Point coordinate) {
+        createItem(item,ability);
         this.coordinate = coordinate;
     }
 
@@ -28,5 +28,86 @@ public class Item {
 
     public void setItem(ItemEnum item) {
         this.item = item;
+    }
+
+    //create getter setter for enhance
+
+
+
+    public void createItem (ItemEnum itemEnum, AbilityEnum abilityEnum){
+        //check if the item is "HELMET"
+        if (itemEnum == ItemEnum.HELMET){
+            if(abilityEnum == AbilityEnum.ARMORCLASS){
+                //Create Helmet
+                this.item = itemEnum;
+                this.ability = abilityEnum;
+                enhance = +1;
+                //save into file
+            }
+            else if(itemEnum == ItemEnum.ARMOMR){
+                if(abilityEnum == AbilityEnum.ARMORCLASS){
+                    //Create ArmorClass
+                    this.item = itemEnum;
+                    this.ability = abilityEnum;
+                    enhance = +1;
+                    //save into file
+                }
+
+            }else if (itemEnum == ItemEnum.SHIELD){
+                if(abilityEnum == AbilityEnum.ARMORCLASS || abilityEnum == AbilityEnum.STRENGTH || abilityEnum == AbilityEnum.CONSTITUTION ){
+                    //Create Shield
+                    this.item = itemEnum;
+                    this.ability = abilityEnum;
+                    enhance = +1;
+                    //save into file
+                }
+            }else if(itemEnum == ItemEnum.RING) {
+                if (abilityEnum == AbilityEnum.ARMORCLASS || abilityEnum == AbilityEnum.STRENGTH || abilityEnum == AbilityEnum.CONSTITUTION) {
+                    //Create Ring
+                    this.item = itemEnum;
+                    this.ability = abilityEnum;
+                    enhance = +1;
+                    //save into file
+                }
+            }else if(itemEnum == ItemEnum.BELT) {
+                if (abilityEnum == AbilityEnum.STRENGTH || abilityEnum == AbilityEnum.CONSTITUTION) {
+                    //Create Belt
+                    this.item = itemEnum;
+                    this.ability = abilityEnum;
+                    enhance = +1;
+                    //save into file
+                }
+            }else if(itemEnum == ItemEnum.BOOTS) {
+                if (abilityEnum == AbilityEnum.ARMORCLASS || abilityEnum == AbilityEnum.DEXTERITY) {
+                    //Create Boots
+                    this.item = itemEnum;
+                    this.ability = abilityEnum;
+                    enhance = +1;
+                    //save into file
+                }
+            }else if (itemEnum == ItemEnum.WEAPON) {
+                if (abilityEnum == AbilityEnum.ATTACKBONUS || abilityEnum == AbilityEnum.DAMAGEBONUS) {
+                    //Crete weapon
+                    this.item = itemEnum;
+                    this.ability = abilityEnum;
+                    enhance = +1;
+                    //save into file
+                }
+            }else {
+                System.out.println("Invalid item");
+            }
+        }
+    }
+
+    public void editItem(boolean remove,AbilityEnum abilityEnum){
+        if(remove){
+            // load and save functions
+        }else {
+            createItem(this.item,abilityEnum);
+        }
+    }
+
+    public void enhancement(int enhance){
+        // character should call this when enters the map to enhance the item regarding to his level
     }
 }
