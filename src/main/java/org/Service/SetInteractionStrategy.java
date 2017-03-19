e835e3f8e297dde569e59bf8f24bd39990ff53ca
Amir Sadra Khorramizadeh <amirsadra@me.com>
2017-03-19 02:17:36 -0400

    Mereged SetInteractionStrategy from Meysam

--- src/main/java/org/Service/SetInteractionStrategy.java
+++ src/main/java/org/Service/SetInteractionStrategy.java
@@ -1,10 +1,12 @@
 package main.java.org.Service;
 
-import main.java.org.model.Campaign;
+import main.java.org.model.*;
 import main.java.org.model.Character;
-import main.java.org.model.Map;
 
 import java.awt.*;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  * this method is to dispatch different behaviour based on the interactions with Monster, ....
@@ -17,7 +19,7 @@ import java.awt.*;
  * @objectCoordinate the coordinate of the object we are interacting with
  */
 public class SetInteractionStrategy {
-    public static void interact(Map map, String targetObject, Point playerCoordinate, Point objectCoordinate, Campaign campaign) {
+    public void interact(Map map, String targetObject, Point playerCoordinate, Point objectCoordinate, Campaign campaign) {
 
         if ("Q".equalsIgnoreCase(targetObject)) {
             goToNextLevel(map, targetObject, playerCoordinate, objectCoordinate, campaign);
@@ -30,32 +32,71 @@ public class SetInteractionStrategy {
         }
 
     }
-
-    private static void interactWithFriendlyCharacter(Map map, String targetObject, Point playerCoordinate, Point objectCoordinate, Campaign campaign) {
-        Character character=null;
+    /**
+     * This method will exchange an item with a friendly monster.
+     */
+    private void interactWithFriendlyCharacter(Map map, String targetObject, Point playerCoordinate, Point objectCoordinate, Campaign campaign) {
+        Character friendlyCharacter=null;
         try {
-            character = ObjectLoader.loadCharacterFromXML(targetObject);
+            friendlyCharacter = ObjectLoader.loadCharacterFromXML(targetObject);
         } catch (Exception e) {
             e.printStackTrace();
         }
         //TODO interactWithFriendlyCharacter here
+        Character character = null;
+
+        List<Item> friendlyCharacterBackpack, playerBackPack;
+        friendlyCharacterBackpack = friendlyCharacter.getBackPackInventory();
+        playerBackPack = character.getBackPackInventory();
+
+        System.out.println("Choose an item to exchange with an item from friendly monster: \n"+playerBackPack.toString());
+        int itemToGive = Integer.parseInt(readLine());
+
+        Item temp1 = playerBackPack.get(itemToGive);
+        playerBackPack.remove(itemToGive);
+
+        Random randomGenerator;
+        randomGenerator = new Random();
+        int index = randomGenerator.nextInt(friendlyCharacterBackpack.size());
+
+        Item itemToReceive = friendlyCharacterBackpack.get(index);
+
+        playerBackPack.add(itemToReceive);
+        friendlyCharacterBackpack.remove(itemToReceive);
+
+        friendlyCharacterBackpack.add(temp1);
     }
 
     private static void interactWithChest(Map map, String targetObject, Point playerCoordinate, Point objectCoordinate, Campaign campaign) {
         //TODO interactWithChest here
+        Character character = null;
+        BackPackInventory chest = null;
+        List<Item> loot;
+        loot = chest.getItems();
+        character.setBackPackInventory((BackPackInventory) loot);
     }
 
     private static void interactWithMonster(Map map, String targetObject, Point playerCoordinate, Point objectCoordinate, Campaign campaign) {
-        Character character=null;
+        Character character = null;
+
+        Character monster=null;
         try {
-            character = ObjectLoader.loadCharacterFromXML(targetObject);
+            monster = ObjectLoader.loadCharacterFromXML(targetObject);
         } catch (Exception e) {
             e.printStackTrace();
         }
         //TODO interactWithMonster here
+        Ability ability = character.getAbility();
+        monster.decreaseHitPoint(ability.getAttackBonus());
+
     }
 
     private static void goToNextLevel(Map map, String targetObject, Point playerCoordinate, Point objectCoordinate, Campaign campaign) {
         //TODO go to next level logic here + adjust the Campaign
+    }
+
+    private String readLine() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
     }
 }
